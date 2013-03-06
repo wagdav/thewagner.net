1a2e003101a326271c1a23e77a9d6f7558e3cf4b
David Wagner <david.wagner@epfl.ch>
2013-03-06 23:03:59 +0100

    Add pelican basis.

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'David'
+SITENAME = u'thewagner.net'
+SITEURL = 'http://thewagner.net'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'hu'
+DEFAULT_CATEGORY = 'blog'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+LINKS = ()
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+#FEED_DOMAIN = 'http://thewagner.net'
