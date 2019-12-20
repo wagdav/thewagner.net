4d879b2f20d7c3eab7cbc3b4f69ab9d0a4014427
David Wagner <wagdav@gmail.com>
2019-12-20 18:13:55 +0100

    Fix links

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,17 @@ from __future__ import unicode_literals
 
 AUTHOR = 'David Wagner'
 SITENAME = 'The Wagner'
-SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en'
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+ARTICLE_LANG_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}/'
+ARTICLE_LANG_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}-{lang}/index.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
