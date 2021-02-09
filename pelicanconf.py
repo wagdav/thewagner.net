09d68766ad2014b486996629a3b77ff9e183ade9
David Wagner <wagdav@gmail.com>
2021-02-09 21:28:27 +0100

    Remove __future__ imports

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 AUTHOR = 'David Wagner'
 SITENAME = 'The Wagner'
 
--- publishconf.py
+++ publishconf.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
 sys.path.append(os.curdir)
