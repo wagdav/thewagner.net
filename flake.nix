db5bc9ede00d5c82ced705deaf02a77fc9600a95
David Wagner <wagdav@gmail.com>
2021-02-13 09:44:49 +0100

    Support build on all default platforms

--- flake.nix
+++ flake.nix
@@ -2,29 +2,25 @@
   description = "The contents of https://thewagner.net";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-20.09";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
 
-      system = "x86_64-linux";
+        system = "x86_64-linux";
 
-      pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      pythonEnv = pkgs.python3.withPackages (ps: with ps; [
-        markdown
-        pelican
-        pygal
-        typogrify
-      ]);
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          markdown
+          pelican
+          pygal
+          typogrify
+        ]);
 
-    in
-    rec {
-
-      defaultPackage."${system}" = packages."${system}".thewagner-net;
-
-      packages."${system}" = {
-        thewagner-net = pkgs.runCommand "pelican"
+        htmlPages = pkgs.runCommand "pelican"
           {
             preferLocalBuild = true;
             buildInputs = [ pythonEnv ];
@@ -42,66 +38,67 @@
               ${./content}
           '';
 
-        ociImage =
-          let
-            port = 8000;
 
-            htmlPages = self.packages."${system}".thewagner-net;
+      in
+      rec {
 
-          in
-          pkgs.dockerTools.buildLayeredImage
-            {
-              name = "thewagner.net";
-              tag = revision;
-              contents = [ pkgs.python3Minimal htmlPages ];
-              config = {
-                Cmd = [
-                  "${pkgs.python3Minimal}/bin/python"
-                  "-m"
-                  "http.server"
-                  (builtins.toString port)
-                  "--directory"
-                  "${htmlPages}"
-                ];
-                Expose = [ port ];
+        defaultPackage = htmlPages;
+
+        packages = {
+          ociImage =
+            let
+              port = 8000;
+
+            in
+            pkgs.dockerTools.buildLayeredImage
+              {
+                name = "thewagner.net";
+                tag = revision;
+                contents = [ pkgs.python3Minimal htmlPages ];
+                config = {
+                  Cmd = [
+                    "${pkgs.python3Minimal}/bin/python"
+                    "-m"
+                    "http.server"
+                    (builtins.toString port)
+                    "--directory"
+                    "${htmlPages}"
+                  ];
+                  Expose = [ port ];
+                };
               };
-            };
 
-      };
+        };
 
-      checks."${system}" = {
+        checks = {
 
-        build = self.defaultPackage."${system}";
+          shellcheck = pkgs.runCommand "shellcheck"
+            {
+              buildInputs = with pkgs; [ shellcheck ];
+            }
+            ''
+              mkdir $out
+              shellcheck --shell bash ${./scripts}/*
+            '';
 
-        buildImage = self.packages."${system}".ociImage;
+          markdownlint = pkgs.runCommand "mdl"
+            {
+              buildInputs = with pkgs; [ mdl ];
+            }
+            ''
+              mkdir $out
+              mdl ${./README.md}
+              mdl ${./content}/2020*
+            '';
 
-        shellcheck = pkgs.runCommand "shellcheck"
-          {
-            buildInputs = with pkgs; [ shellcheck ];
-          }
-          ''
-            mkdir $out
-            shellcheck --shell bash ${./scripts}/*
-          '';
-
-        markdownlint = pkgs.runCommand "mdl"
-          {
-            buildInputs = with pkgs; [ mdl ];
-          }
-          ''
-            mkdir $out
-            mdl ${./README.md}
-            mdl ${./content}/2020*
-          '';
-
-        yamllint = pkgs.runCommand "yamllint"
-          {
-            buildInputs = with pkgs; [ yamllint ];
-          }
-          ''
-            mkdir $out
-            yamllint --strict ${./.github/workflows}
-          '';
-      };
-    };
+          yamllint = pkgs.runCommand "yamllint"
+            {
+              buildInputs = with pkgs; [ yamllint ];
+            }
+            ''
+              mkdir $out
+              yamllint --strict ${./.github/workflows}
+            '';
+        };
+      });
 }
