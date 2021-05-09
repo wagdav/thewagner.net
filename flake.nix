758421082f6e3b9cae2109703a24d5e135bc6420
David Wagner <wagdav@gmail.com>
2021-05-09 18:00:12 +0200

    Markdownlint now checks every article

--- flake.nix
+++ flake.nix
@@ -94,8 +94,7 @@
             ''
               mkdir $out
               mdl ${./README.md}
-              mdl ${./content}/2020*
-              mdl ${./content}/2021*
+              mdl ${./content}
             '';
 
           yamllint = pkgs.runCommand "yamllint"
