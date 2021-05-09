0fd4fe6f990ec968e69258451cc2182ff03df3a7
David Wagner <wagdav@gmail.com>
2021-05-09 18:53:50 +0200

    Switch to a single repository setup
    The primary branch is now `main` and the the https://thewagner.net is
    deployed from the `gh-pages` branch.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,16 +3,6 @@
 
 set -eu
 
-USER=wagdav
-REPO=wagdav.github.com
-
-if [ -z ${DEPLOY_TOKEN+x} ]; then
-    GITHUB=git@github.com:$USER/$REPO
-else
-    GITHUB=https://$USER:"$DEPLOY_TOKEN"@github.com/$USER/$REPO
-fi
-
-GITHUB_PAGES_BRANCH=gh-pages
 nix-build
-ghp-import -m "Automatic update" -b $GITHUB_PAGES_BRANCH ./result
-git push -f "$GITHUB" $GITHUB_PAGES_BRANCH:master
+ghp-import -m "Automatic update" ./result
+git push origin gh-pages
