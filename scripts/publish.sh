85f1f25ae8a9153f6ed5a2594518278fa694f217
David Wagner <wagdav@gmail.com>
2021-05-19 13:23:52 +0200

    Fix publish.sh: force push to the gh-pages branch

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,4 +5,4 @@ set -eu
 
 SITE=$(nix-build --no-out-link -A packages.x86_64-linux.site)
 ghp-import -m "Automatic update" "$SITE"
-git push origin gh-pages
+git push --force origin gh-pages:gh-pages
