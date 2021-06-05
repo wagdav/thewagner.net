a6a2691ec1020b137fb9f182b1e766abcc3bf491
David Wagner <wagdav@gmail.com>
2021-06-05 15:48:44 +0200

    Mention the source repository

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,5 @@
 set -eu
 
 SITE=$(nix-build --no-out-link -A packages.x86_64-linux.site)
-ghp-import -m "Automatic update" "$SITE"
+ghp-import --message "Automatic update from https://github.com/wagdav/wagdav.github.com" "$SITE"
 git push --force origin gh-pages:gh-pages
