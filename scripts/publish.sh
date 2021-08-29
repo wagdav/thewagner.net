f0d6b5dd25485dcdaef0591b419a7c6f0631baa7
David Wagner <wagdav@gmail.com>
2021-08-29 14:48:51 +0200

    Change source repository name

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,5 @@
 set -eu
 
 SITE=$(nix-build --no-out-link -A packages.x86_64-linux.site)
-ghp-import --message "Automatic update from https://github.com/wagdav/wagdav.github.com" "$SITE"
+ghp-import --message "Automatic update from https://github.com/wagdav/thewagner.net" "$SITE"
 git push --force origin gh-pages:gh-pages
