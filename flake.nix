43a78c89d843aea256fe71441822b88a06587e5b
David Wagner <wagdav@gmail.com>
2022-06-28 20:05:48 +0200

    Use new-style flake-compat

--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat = {
+    url = github:edolstra/flake-compat;
+    flake = false;
+  };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem
+  outputs = { self, nixpkgs, flake-utils, flake-compat }: flake-utils.lib.eachDefaultSystem
     (system:
       let
         revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
