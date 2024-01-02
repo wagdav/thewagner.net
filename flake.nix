7b78034d4ee0b003c0ac291dca36c343ccb3115e
David Wagner <wagdav@gmail.com>
2024-01-02 11:34:32 +0100

    Update to nixpkgs/nixos-23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The contents of https://thewagner.net";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-compat = {
     url = github:edolstra/flake-compat;
