ec118cdc15bafc9262bebc22a73528be0e12194a
David Wagner <wagdav@gmail.com>
2024-10-13 13:23:30 +0200

    Check style: use languagetool

--- scripts/check_style.sh
+++ scripts/check_style.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 nix shell nixpkgs#pandoc -c pandoc "$1" --to plain | \
-nix shell nixpkgs#diction -c diction --suggest --beginner
+nix shell nixpkgs#languagetool -c languagetool-commandline \
+  --language en-US \
+  -
